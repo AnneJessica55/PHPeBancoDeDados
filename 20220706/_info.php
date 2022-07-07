efa1f54ef8d7c4d305d2612cf0da9e32d93aaea0
AnneJessica55 <annejessica55@gmail.com>
2022-07-07 02:10:43 -0300

    20220706

---
+++ 20220706/_info.php
@@ -0,0 +1,10 @@
+<pre>
+    <!--Essa tag serve para pré formatação. Toda a formatação do editor será repetida na página-->
+    <!--O "print_r" serve para exibir o conteúdo de uma variável
+    Aula 06 do canal Programação web: https://youtu.be/pN0wEENSFpY -->
+    <?php print_r($_SERVER); 
+    /*A variável $_SERVER é uma variável superglobal. Uma variável do próprio PHP que contém algumas informações importantes do site.
+    Para saber mais sobre as superglobais, assista a aula 17 do canal Programação Web - https://youtu.be/-_sG4VbC-pI
+    [DOCUMENT_ROOT] => C:/xampp/htdocs é o caminho do arquivo*/
+    ?>
+</pre>
